21413b6c5eab15f828d1cd52badd8ed38c683af1
(Simon Martinez) <smartineze1993@gmail.com>
2020-12-23 10:31:25 -0500

    deleted pho_number from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_050737) do
+ActiveRecord::Schema.define(version: 2020_12_23_152945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2020_12_22_050737) do
   create_table "forms", force: :cascade do |t|
     t.string "name"
     t.string "last_name"
-    t.integer "phone_number"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
