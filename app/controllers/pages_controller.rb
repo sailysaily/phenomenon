d9d025f2bc5687dc5dce16420bcbf2a36dc1bd3c
(Simon Martinez) <smartineze1993@gmail.com>
2021-03-24 16:15:06 -0500

    Created pages controller, services, contact and team views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+def services
+end
+
+def team
+end
+
+def contact
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root to: 'forms#index'
 
   resources :forms, except: [:index]
+
+  get '/services', to: "pages#services"
+  get '/team', to: "pages#team"
+  get '/contact', to: "pages#contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
